60789db3428d0f975208ac7c0d89c7942d046053
jonasbfranco <jonasbfranco@hotmail.com>
2026-09-10 12:35:39 -0300

    :sparkles: vercel.json simplificado

--- src/config/db.js
+++ src/config/db.js
@@ -7,12 +7,18 @@ const { Pool } = pg;
 /* const pool = new Pool({
     connectionString: process.env.DATABASE_URL
 }); */
-
+/* 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: process.env.NODE_ENV === "production"
     ? { rejectUnauthorized: false }
     : false
-});
+}); */
+
+const cpool = new Pool({
+  process.env.DATABASE_URL ||
+  process.env.POSTGRES_URL
+})
+
 
 export default pool;
