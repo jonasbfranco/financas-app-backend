01dbd87ce19d3f1889a97697aeb8c85c1b71611e
jonasbfranco <jonasbfranco@hotmail.com>
2026-09-10 15:04:21 -0300

    :sparkles: DIRECT-URL

--- src/config/db.js
+++ src/config/db.js
@@ -5,10 +5,10 @@ import pg from "pg";
 const { Pool } = pg;
 
 /* const pool = new Pool({
-    connectionString: process.env.DATABASE_URL
+    connectionString: process.env.DATABASE_URL 
 }); */
-/* 
-const pool = new Pool({
+ 
+/* const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: process.env.NODE_ENV === "production"
     ? { rejectUnauthorized: false }
@@ -16,6 +16,7 @@ const pool = new Pool({
 }); */
 
 const connectionString =
+  process.env.DIRECT_URL ||
   process.env.DATABASE_URL ||
   process.env.POSTGRES_URL;
 
